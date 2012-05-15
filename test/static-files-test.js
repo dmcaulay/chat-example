9ebd5ced6e357bd03dced51bf0fcabdff31f2ed6
Dan McAulay <dmcaulay@gmail.com>
2012-05-15 00:20:13 -0700

    first test passes

--- test/static-files-test.js
+++ test/static-files-test.js
@@ -5,13 +5,17 @@ var http = require('http')
 
 describe('static', function() {
   var port = 8081;
+  var server = null;
   before(function() {
     server = http.createServer(staticFiles);
     server.listen(port);
   });
+  after(function() {
+    server.close();
+  });
   it('serves static files', function(done) {
-    request({uri: "http://localhost:' + port + '/todo.txt"}, function(err, res, body) {
-      body.should.equal('- groceries');
+    request({uri: 'http://localhost:' + port + '/todo.txt'}, function(err, res, body) {
+      body.should.equal('- groceries\n');
       done();
     });
   });
