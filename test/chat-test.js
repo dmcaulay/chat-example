4fc592380afed4b5b28f442e53e6876b51bfdf9a
Dan McAulay <dmcaulay@gmail.com>
2012-05-15 03:25:22 -0700

    server side socket.io is tested

--- test/chat-test.js
+++ test/chat-test.js
@@ -4,12 +4,32 @@ var io = require('socket.io-client')
 
 describe('chat', function() {
   var port = 8888;
-  it('notifies the users when someone joins the chat', function(done) {
+  var joe = null;
+  var jon = null;
+  var jane = null;
+  before(function() {
     var server = chat(port);
-    var joe = io.connect('http://localhost:' + port, {'force new connection': true});
-    var jon = io.connect('http://localhost:' + port, {'force new connection': true});
-
-    // we're done after joe and jon are both notified
+  });
+  beforeEach(function(done) {
+    var count = 0;
+    function connected() {
+      count += 1;
+      if (count == 3) done();
+    }
+    joe = io.connect('http://localhost:' + port, {'force new connection': true});
+    jon = io.connect('http://localhost:' + port, {'force new connection': true});
+    jane = io.connect('http://localhost:' + port, {'force new connection': true});
+    joe.on('connect', connected);
+    jon.on('connect', connected);
+    jane.on('connect', connected);
+  });
+  afterEach(function() {
+    joe.disconnect();
+    jon.disconnect();
+    jane.disconnect();
+  });
+  it('notifies the users when someone joins the chat', function(done) {
+    // we're done after joe and jon are notified
     var notification = 0;
     function joined(data) {
       notification += 1;
@@ -19,7 +39,34 @@ describe('chat', function() {
 
     joe.on('joined', joined);
     jon.on('joined', joined);
-    var jane = io.connect('http://localhost:' + port, {'force new connection': true});
     jane.emit('joined', {username: 'jane'});
+  });
+  it('notifies the users when someone sends a message', function(done) {
+    // we're done after jane and jon are notified
+    var notification = 0;
+    function message(data) {
+      notification += 1;
+      data.username.should.equal('joe');
+      data.message.should.equal("yo what's up");
+      if (notification == 2) done();
+    }
+
+    jon.on('message', message);
+    jane.on('message', message);
+    joe.emit('message', {username: 'joe', message: "yo what's up"});
+  });
+  it('notifies the users when someone leaves the chat', function(done) {
+    // we're done after jane and joe are notified
+    var notification = 0;
+    function left(data) {
+      notification += 1;
+      data.username.should.equal('jon');
+      if (notification == 2) done();
+    }
+
+    joe.on('left', left);
+    jane.on('left', left);
+    jon.emit('joined', {username: 'jon'});
+    jon.disconnect();
   });
 });
