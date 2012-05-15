9de35a884beab61d103f5862fb6a7d6f2cf50a72
Dan McAulay <dmcaulay@gmail.com>
2012-05-15 03:29:19 -0700

    adding chat implementation to the server

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var http = require('http')
-  , staticFiles = require('./lib/static-files')(__dirname + '/public');
+  , staticFiles = require('./lib/static-files')(__dirname + '/public')
+  , chat = require('./lib/chat');
 
 var server = http.createServer(staticFiles);
 server.listen(8080);
+
+chat(server);
